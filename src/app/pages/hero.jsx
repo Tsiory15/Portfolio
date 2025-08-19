9cecf8eddbda55aabe714d2e79aaf6442bbfccd4
unknown <raphaeltsiory15@gmail.com>
2025-08-19 12:04:19 +0200

    some adjustment

--- src/app/pages/hero.jsx
+++ src/app/pages/hero.jsx
@@ -9,23 +9,11 @@ import Pic2 from '../../../public/Leonardo_Phoenix_10_A_person_with_a_futuristic
 import Link from 'next/link';
 import Menu from "../components/menu";
 import { useGSAP } from '@gsap/react';
-import { useEffect, useRef, useState } from 'react';
-// import {
-//     Carousel,
-//     CarouselContent,
-//     CarouselItem,
-//     CarouselNext,
-//     CarouselPrevious
-// } from '@/components/ui/carousel'
-import  { CgClose } from 'react-icons/cg';
 import { FaGithub,FaPhone } from 'react-icons/fa';
 // import { MailIcon } from 'lucide-react';
 
-const imgs = ['1','2','3','4','5','6','7','8','9','10','11','12','13']
-const scrn = ['r1','r2','r3']
 export default function Hero(){
-    const [pj1,setPj1] = useState(true)
-    const [pj2,setPj2] = useState(true)
+    
     gsap.registerPlugin(ScrollTrigger,Draggable,Observer,ScrollToPlugin,CustomEase,ScrollSmoother)
 useGSAP(() =>{
     gsap.to('.services_text',{
@@ -113,82 +101,6 @@ useGSAP(() =>{
 },[])
     return (
         <div>
-            <div className={pj1 ? 'hidecase' : 'showcase'}>
-                <div className="closebtn" onClick={()=>setPj1(true)}>
-                    <CgClose></CgClose>
-                    </div>
-                    <div className="project_content">
-                    {/* <Carousel className='slider' opts={{loop:true}}>
-                        <CarouselContent>
-                            {imgs.map((img,index) => (
-                            <CarouselItem style={{height:'100%',display:'flex',alignItems:'center',justifyContent:'center'}} key={index}>
-                                <Image
-                            src={`/zarahay/${img}.png`}
-                            alt=''
-                            width={900}
-                            height={100}
-                            />
-                            </CarouselItem>
-                            ))}
-                            
-                        </CarouselContent>
-                    </Carousel> */}
-                </div>
-                <div className="description">
-                    This project is an online training platform made for Zarahay doctorant community. <br /> It aims to provide members
-                    with a smooth and engaging learning experience <br /> through a variety of courses.
-                    <br /> 
-                    <ul>
-                        Key features :
-                        <li>Secure registration and authentication</li>
-                        <li>Course with video and downloadable file</li>
-                        <li>Upload and authorization management for admin account</li>
-                        <li>Course catalog</li>
-                    </ul>
-                    <ul>
-                        <div>Technologie used : </div>
-                        <li>React,Node.js and MySQL</li>
-                    </ul>
-                </div>
-            </div>
-            <div className={pj2 ? 'hidecase' : 'showcase'}>
-                <div className="closebtn" onClick={()=>setPj2(true)}> 
-                    <CgClose></CgClose>
-                    </div>
-                    <div className="project_content">
-                        {
-                    //<Carousel className='slider' opts={{loop:true}}>
-                      //  <CarouselContent>
-                        //    {scrn.map((img,index) => (
-                          //  <CarouselItem style={{width:'100%',height:'100%',display:'flex',alignItems:'center',justifyContent:'center'}} key={index}>
-                            //    <Image
-                           // src={`/relaxation/${img}.png`}
-                           // alt=''
-                           // width={300}
-                           // height={100}
-                           // />
-                           // </CarouselItem>
-                          //  ))}
-                            
-                       // </CarouselContent>
-                   // </Carousel> */
-                   }
-                </div>
-                <div className="description">
-                    This project is a mobile application designed to help users reduce stress, <br />
-                    improve focus, and enhance overall well-being through guided meditation techniques
-                    <br /> 
-                    <ul>
-                        Key features :
-                        <li>Relaxing music</li>
-                        <li>Guided meditation and breathing exercices for stress relief, sleep and focus</li>
-                    </ul>
-                    <ul>
-                        <div>Technologie used : </div>
-                        <li>React native</li>
-                    </ul>
-                </div>
-            </div>
             <Menu/>
             <div>
             <div className= "main_container">
@@ -197,7 +109,7 @@ useGSAP(() =>{
                         <div className="backdrop"></div>
                         <div><p className='itext'>THINK.</p></div><div><p className='itext'>CODE.</p></div><div><p className='itext' >DEPLOY.</p></div>
                     </div>
-                    {/* TEMPORARY */}
+
                     <div className="service_container">
                         <div className="services">
                             <div className="services_text">
@@ -209,13 +121,10 @@ useGSAP(() =>{
                                 
                             </div>
                             <div className='text'>
-                                Hello, I'm Tsiory Raphaël a web developer. <br />
+                                Hello, I'm Tsiory Raphaël a web developer.
                                 Passionate about creating modern, responsive, and user-friendly websites.
                                 <br />
                                <br /> Skilled in HTML,CSS,JavaScript,React,Next js and Node.js and always eager to learn new technologies to push my limits
-                                <div className="download_button">
-                                   <a href='../../../CV_RAVELONOMENAJANAHARY_Tsiory_Raphael.pdf'>Curriculum vitae</a>
-                                </div>
                             </div>
                         </div>
                     </div>
@@ -230,7 +139,7 @@ useGSAP(() =>{
                             <span className='e'>E</span>
                             <span className='c'>C</span>
                             <span className='t'>T</span>
-                        <div className="project1 pj" onClick={()=>setPj1(false)}>
+                        <div className="project1 pj">
                             <div className="card">
                                 <p className='title'>Zarahay doctorant</p>
                             </div>
@@ -240,7 +149,7 @@ useGSAP(() =>{
                             className='image pji'
                             />
                         </div>
-                       <div className="project2 pj" onClick={()=>setPj2(false)}>
+                       <div className="project2 pj">
                         <Image
                             src={Pic2}
                             alt=''
